c03d75f968f93d57cef9f804484f22d3dcf70be1
Phillip Mortari <pmortari@gmail.com>
2026-02-25 14:44:12 -0300

    Initial version

---
+++ KeepAwake.bat
@@ -0,0 +1,1 @@
+powershell.exe -file KeepAwake.ps1
---
+++ KeepAwake.ps1
@@ -0,0 +1,7 @@
+﻿$wsh = New-Object -ComObject WScript.Shell
+
+while ($true) {
+    $wsh.SendKeys('+{F15}')
+    $time = Get-Random -Minimum 55 -Maximum 86
+    Start-Sleep -Seconds $time
+}
